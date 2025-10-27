6efdb0822e5fe20d4740c4d773285a92921de930
Kasim Fejzic <kasimfejzo@gmail.com>
2025-10-27 21:11:08 +0100

    chore: move from main to lib

---
+++ trafix-codec/src/lib.rs
@@ -0,0 +1,7 @@
+#![warn(clippy::pedantic)]
+#![forbid(unsafe_code)]
+
+//! `trafix-codec` is a low-level library for high-performance parsing,
+//! encoding, and validation of FIX messages.
+
+pub mod encoder;
